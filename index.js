a081b6088400a323b0ac7eed152cc514f104db0c
Mayk Brito <77559483+maykrbrito@users.noreply.github.com>
2022-06-09 11:53:20 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+const elementoResposta = document.querySelector("#resposta")
+const inputPergunta = document.querySelector("#inputPergunta")
+const buttonPerguntar = document.querySelector('#buttonPerguntar')
+const respostas = [
+  "Certeza!",
+  "Não tenho tanta certeza.",
+  "É decididamente assim.",
+  "Não conte com isso.",
+  "Sem dúvidas!",
+  "Pergunte novamente mais tarde.",
+  "Sim, definitivamente!",
+  "Minha resposta é não.",
+  "Você pode contar com isso.",
+  "Melhor não te dizer agora.",
+  "A meu ver, sim.",
+  "Minhas fontes dizem não.",
+  "Provavelmente.",
+  "Não é possível prever agora.",
+  "Perspectiva boa.",
+  "As perspectivas não são tão boas.",
+  "Sim.",
+  "Concentre-se e pergunte novamente.",
+  "Sinais apontam que sim.",
+]
+
+// clicar em fazer pergunta
+function fazerPergunta() {
+
+  if(inputPergunta.value == "") {
+    alert("Digite sua pergunta")
+    return
+  }
+
+  buttonPerguntar.setAttribute("disabled", true)
+
+  const pergunta = "<div>" + inputPergunta.value + "</div>"
+
+  // gerar numero aletorio
+  const  totalRespostas = respostas.length
+  const numeroAleatorio = Math.floor(Math.random() * totalRespostas)
+
+  elementoResposta.innerHTML = pergunta + respostas[numeroAleatorio]
+
+  elementoResposta.style.opacity = 1;
+
+  // sumir a resposta depois de 3 segundos
+  setTimeout(function() {
+    elementoResposta.style.opacity = 0;
+    buttonPerguntar.removeAttribute("disabled")
+  }, 3000)
+}
---
+++ readme.md
@@ -0,0 +1,31 @@
+# HTML
+- HyperText Markup Language
+
+- Hiper Texto?
+- Marcação
+  - tags
+  - atributos
+- Linguagem
+  - maneira de escrever
+
+# CSS
+
+- Apresentação visual para o cliente
+- Estilos para o HTML
+- Cascading Style Sheets
+  - Folha de Estilo em Cascata
+
+# Declaração
+- Seletor
+- Propriedade e Valor
+
+# Conceitos
+- Cascata
+- Especificidade
+- Box Model
+- Display block vs inline
+
+
+
+### imagem bola de cristal
+https://gist.githubusercontent.com/maykbrito/0acdf4ce919838ffed50915a31fc5b23/raw/6f4dd01ec3116428ec4c99255944cb9ac7927590/cristal-ball.svg
